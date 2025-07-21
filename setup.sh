129400ab97b5a2225faaac27449288b992ae800e
Daniel Bachhuber <daniel@newpublic.org>
2025-07-21 06:11:56 -0700

    Add Ghostty config

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ ln -sf $DIR/zshrc $DIR/../.zshrc
 ln -sf $DIR/vimrc $DIR/../.vimrc
 rm -rf $DIR/../.vim; ln -sf $DIR/vim $DIR/../.vim
 ln -sf $DIR/bashrc $DIR/../.bashrc
+mkdir -p $HOME/.config/ghostty/
+ln -sf $DIR/ghostty $HOME/.config/ghostty/config
